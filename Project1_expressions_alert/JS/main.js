947df03be7aac911aee41c4f0a5125cb41cabd52
b97-m <102753461+b97-m@users.noreply.github.com>
2022-08-07 21:40:11 +0100

    First steps in JS

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,14 @@
+var A = "Hello, World!";                                //assaigning a variable A with a string
+window.alert(A);                                        //printing this variable on the page in a pop-up message (alert box)
+
+var B = "Welcome to my first page in JavaScript. ";     //assigning a variable B with a string
+document.write(B);                                      //printing this variable on the actual page
+
+
+var C = "My name is";                                   //assigning a variable C with a string
+var D = " Barbara. ";                                   //assigning a variable D with a string
+var D = D.fontcolor("pink");                            //assigning a color to variable D
+document.write(C + D);                                  //concatenating variables C and D
+
+
+document.write("x + 3")                                 //expression
